f44c1d259f48b60b71bcacf7a13eed28826f9c4f
Stepan Fedyanov <fedyanov@artlebedev.ru>
2023-11-29 10:45:28 +0700

    Change vite config for production

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: process.env.NODE_ENV === "production" ? "/sf/" : "/",
+  base: import.meta.env.PROD ? "/sf/" : "/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
