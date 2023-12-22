20aeca43ce9681713f337ad77ef3ad329ffb8992
Stepan Fedyanov <fedyanov@artlebedev.ru>
2023-12-22 16:07:55 +0300

    Change base URL

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  base: process.env.NODE_ENV === 'production' ? '/sf' : '',
   css: {
     preprocessorOptions: {
       scss: {
